86c88fb888ccb8e3ba884975450a66d74ed0b6c8
Matt Cosand <matt@cosand.net>
2018-12-02 21:26:13 -0800

    day 3 part 2

--- day-03/Program.cs
+++ day-03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -10,26 +11,47 @@ namespace day_03
     static void Main(string[] args)
     {
       int[][] squares = new int[1000][];
+      Dictionary<int, bool> nonconflicts = new Dictionary<int, bool>();
+
       for (int i=0;i<1000;i++)
       {
         squares[i] = new int[1000];
       }
 
-      foreach (var m in File.ReadAllLines("input.txt").Select(f => Regex.Match(f, "(\\d+),(\\d+): (\\d+)x(\\d+)")))
+
+      foreach (var m in File.ReadAllLines("input.txt").Select(f => Regex.Match(f, "(\\d+) @ (\\d+),(\\d+): (\\d+)x(\\d+)")))
       {
-        int x = int.Parse(m.Groups[1].Value);
-        int y = int.Parse(m.Groups[2].Value);
-        for (int h = 0; h < int.Parse(m.Groups[4].Value); h++)
+        int id = int.Parse(m.Groups[1].Value);
+        int x = int.Parse(m.Groups[2].Value);
+        int y = int.Parse(m.Groups[3].Value);
+        bool conflicts = false;
+        for (int h = 0; h < int.Parse(m.Groups[5].Value); h++)
         {
-          for (int w = 0; w < int.Parse(m.Groups[3].Value); w++)
+          for (int w = 0; w < int.Parse(m.Groups[4].Value); w++)
           {
-            squares[x + w][y + h]++;
+            int oldclaim = squares[x + w][y + h];
+            if (oldclaim == 0)
+            {
+
+            }
+            else
+            {
+              conflicts = true;
+
+              if (nonconflicts.ContainsKey(oldclaim))
+              {
+                conflicts = true;
+                nonconflicts.Remove(oldclaim);
+              }
+            }
+            squares[x + w][y + h] = id;
           }
         }
 
+        if (!conflicts) nonconflicts.Add(id, true);
 
       }
-      int result = squares.SelectMany(f => f).Count(g => g > 1);
+      var result = nonconflicts.Keys;
       Console.WriteLine(result);
     }
   }
