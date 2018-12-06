ffe16423f466b749278f657f804d3add718d9a08
Matt Cosand <matt@cosand.net>
2018-12-06 01:53:52 -0800

    day 6 part 2. ooph

--- day-06/Program.cs
+++ day-06/Program.cs
@@ -9,130 +9,69 @@ namespace day_06
   {
     List<Tuple<int, int>> location = new List<Tuple<int, int>>();
 
-    int[] sizes;
-    int minx = int.MaxValue;
-    int miny = int.MaxValue;
-    int maxx = int.MinValue;
-    int maxy = int.MinValue;
-
     static void Main(string[] args)
     {
       new Program().run();
     }
 
-    int[][] field;
 
     void run() {
+      var print = true;
+      var limit = 32;
       var input = new string[] { "1,1", "1,6", "8,3", "3,4", "5,5", "8,9" };
+
+      print = false;
+      limit = 10000;
       input = File.ReadAllLines("input.txt");
 
       foreach (var line in input)
       {
         var parts = line.Split(",").Select(f => int.Parse(f.Trim())).ToArray();
         location.Add(new Tuple<int, int>(parts[0], parts[1]));
-        if (parts[0] < minx) minx = parts[0];
-        if (parts[0] > maxx) maxx = parts[0];
-        if (parts[1] < miny) miny = parts[1];
-        if (parts[1] > maxy) maxy = parts[1];
-      }
-      sizes = new int[location.Count()];
-
-      field = new int[maxy + 1][];
-      for (var i = 0; i < field.Length; i++)
-      {
-        field[i] = new int[maxx + 1];
-        Array.Fill(field[i], -1);
-      }
-      for (var i = 0; i < location.Count(); i++)
-      {
-        field[location[i].Item2][location[i].Item1] = 10000 + i;
       }
 
-      int dist = 0;
-      bool growth;
+      List<Tuple<int, int>> wavefront = new List<Tuple<int, int>>();
+      List<Tuple<int, int>> nextWave = new List<Tuple<int, int>>();
+
+      wavefront.Add(new Tuple<int, int>((int)location.Select(f => f.Item1).Average(), (int)location.Select(f => f.Item2).Average()));
+      Dictionary<string, bool> hits = new Dictionary<string, bool>();
+      hits.Add($"{wavefront[0].Item1}-{wavefront[0].Item2}", true);
+      showPoint(print, wavefront[0].Item2, wavefront[0].Item2);
+
       do
       {
-        growth = false;
-        dist++;
-
-        Dictionary<string, Tuple<int, int, int>> claims = new Dictionary<string, Tuple<int, int, int>>();
-
-        for (var d = 0; d < dist; d++)
+        nextWave.Clear();
+        foreach (var photon in wavefront)
         {
-          checkClaims(claims, d, d - dist);
-          checkClaims(claims, dist - d, d);
-          checkClaims(claims, -d, dist - d);
-          checkClaims(claims, d - dist, -d);
-        }
-
-
-        foreach (var claim in claims.Values)
-        {
-          if (claim.Item3 >= 0)
+          for (int dx = -1; dx < 2; dx++)
           {
-            field[claim.Item2][claim.Item1] = claim.Item3;
-
-            if (claim.Item1 == minx || claim.Item1 == maxx || claim.Item2 == miny || claim.Item2 == maxy)
+            for (int dy = -1; dy < 2; dy++)
             {
-              sizes[claim.Item3] = int.MinValue;
-              continue;
+              if (dx == 0 && dy == 0) continue;
+
+              int x = photon.Item1 + dx;
+              int y = photon.Item2 + dy;
+
+              string key = $"{x}-{y}";
+              if (!hits.ContainsKey(key) && location.Select(f => Math.Abs(f.Item1 - x) + Math.Abs(f.Item2 - y)).Sum() < limit)
+              {
+                showPoint(print, x, y);
+                hits.Add(key, true);
+                nextWave.Add(new Tuple<int, int>(photon.Item1 + dx, photon.Item2 + dy));
+              }
             }
-
-            sizes[claim.Item3]++;
-            growth = true;
-          }
-          else
-          {
-            field[claim.Item2][claim.Item1] = -2;
           }
         }
 
-   //     print();
-      } while (growth);
+        wavefront = nextWave.ToList();
+      } while (nextWave.Count > 0);
 
-      var big = sizes.Select((f, i) => new { i = i, v = f + 1 }).OrderByDescending(f => f.v).First();
-      Console.WriteLine(big.v);
+      Console.WriteLine(hits.Count);
     }
 
-    void checkClaims(Dictionary<string, Tuple<int, int, int>> claims, int dx, int dy)
+    private static void showPoint(bool print, int x, int y)
     {
-      for (var i = 0; i < location.Count; i++)
-      {
-        int x = location[i].Item1 + dx;
-        int y = location[i].Item2 + dy;
-
-        if (x < minx || x > maxx || y < miny || y > maxy) { continue; }
-
-        int o = field[y][x];
-        // if it's not claimed by a shorter distance
-        if (o == -1)
-        {
-          // see if someone's trying to claim it this round
-          if (claims.TryGetValue($"{x}-{y}", out Tuple<int, int, int> claim))
-          {
-            // If someone's trying to claim it, invalidate the claim
-            claims[$"{x}-{y}"] = new Tuple<int, int, int>(x, y, int.MinValue);
-          }
-          else
-          {
-            claims.Add($"{x}-{y}", new Tuple<int, int, int>(x, y, i));
-          }
-        }
-      }
-    }
-    
-    void print()
-    {
-      Console.SetCursorPosition(0, 0);
-      for (int y=miny; y<= maxy; y++)
-      {
-        for (int x=minx; x <= maxx; x++)
-        {
-          int v = field[y][x];
-          Console.Write(v == -1 ? '.' : v == -2 ? 'x' : v >= 10000 ? (char)('A' + v - 10000) : (char)('a' + v));
-        }
-        Console.WriteLine();
-      }
+      if (print) { Console.SetCursorPosition(x, y); Console.Write('#'); }
     }
   }
 }
