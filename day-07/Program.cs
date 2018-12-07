36b66527d3668055444a37b7c898c264cc6fac40
Matt Cosand <matt@cosand.net>
2018-12-07 09:36:36 -0800

    day 7 part 2

--- day-07/Program.cs
+++ day-07/Program.cs
@@ -11,18 +11,16 @@ namespace day_07
     public List<Step> Opens = new List<Step>();
     public List<Step> Prereqs = new List<Step>();
     public bool Done = false;
+    public int Finishes = 0;
   }
 
   class Program
   {
     static SortedDictionary<string, Step> available = new SortedDictionary<string, Step>();
     static Dictionary<string, Step> steps = new Dictionary<string, Step>();
-    static void Main(string[] args)
-    {
-      Dictionary<string, bool> isDependent = new Dictionary<string, bool>();
 
-      var input = new string[]
-      {
+    static string[] input = new string[]
+    {
         "Step C must be finished before step A can begin.",
         "Step C must be finished before step F can begin.",
         "Step A must be finished before step B can begin.",
@@ -30,8 +28,18 @@ namespace day_07
         "Step B must be finished before step E can begin.",
         "Step D must be finished before step E can begin.",
         "Step F must be finished before step E can begin."
-      };
+    };
+    static Step[] workers = new Step[2];
+    static int clock = 0;
+    static int baseTime = 0;
+
+    static void Main(string[] args)
+    {
+      Dictionary<string, bool> isDependent = new Dictionary<string, bool>();
+
       input = File.ReadAllLines("input.txt");
+      workers = new Step[5];
+      baseTime = 60;
 
       foreach (var line in input.Distinct())
       {
@@ -62,24 +70,48 @@ namespace day_07
       {
         available.Add(start.Name, start);
       }
-      
-      print();
+
+      while (!steps.Values.All(f => f.Done))
+      {
+        workTick();
+        dump();
+        clock++;
+      }
+      Console.WriteLine(clock - 1);
     }
 
-    static void print()
-    {      
-      if (available.Count == 0) return;
-
-      Step current = available[available.Keys.First()];
-      Console.Write(current.Name);
-      current.Done = true;
-      available.Remove(current.Name);
-      foreach (Step next in current.Opens) {
-        if (next.Done) continue;
-        if (!next.Prereqs.All(f => f.Done)) continue;
-        available.TryAdd(next.Name, next);
+    static void workTick()
+    {
+      for (var i=0; i<workers.Length; i++)
+      {
+        if (workers[i] == null) continue;
+        if (workers[i].Finishes == clock)
+        {
+          workers[i].Done = true;
+          foreach (var next in workers[i].Opens)
+          {
+            available.TryAdd(next.Name, next);
+          }
+          workers[i] = null;
+        }
       }
-      print();
+
+      for (var i=0;i<workers.Length;i++)
+      {
+        if (workers[i] == null)
+        {
+          workers[i] = available.Values.Where(f => !f.Done && f.Prereqs.All(g => g.Done)).FirstOrDefault();
+          if (workers[i] == null) continue;
+          available.Remove(workers[i].Name);
+          workers[i].Finishes = clock + baseTime + (workers[i].Name[0] - '@');
+        }
+      }
+
+    }
+
+    static void dump()
+    {
+      Console.WriteLine($"{clock}\t{workers.Aggregate("", (accum, f) => accum + (f == null ? "." : f.Name) + "\t")}{string.Join("", steps.Values.Where(f => f.Done).OrderBy(f => f.Finishes).ThenBy(f => f.Name).Select(f => f.Name))}");
     }
   }
 }
