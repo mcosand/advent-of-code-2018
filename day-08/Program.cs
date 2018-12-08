9af576d0da1b8852def6bf054bebe1d77f1d9861
Matt Cosand <matt@cosand.net>
2018-12-07 22:11:28 -0800

    day 8 part 2

--- day-08/Program.cs
+++ day-08/Program.cs
@@ -8,7 +8,6 @@ namespace day_08
   class Program
   {
     int[] array;
-    List<int> metadata = new List<int>();
 
     static void Main(string[] args)
     {
@@ -21,29 +20,40 @@ namespace day_08
       input = File.ReadAllText("input.txt");
 
       array = input.Split(" ").Select(f => int.Parse(f)).ToArray();
-      readNode(0);
-      int sum = metadata.Sum();
-      Console.WriteLine(sum);
+      var root = readNode(0);
+      Console.WriteLine(root.Item2);
+      var answer = root.Item2;
     }
 
-    int readNode(int start) {
+    Tuple<int,int> readNode(int start) {
       Console.WriteLine("Start reading at " + start);
+
+      int[] childValues = new int[array[start]];
 
       int offset = 0;
       for (int i=0; i<array[start]; i++)
       {
-        offset += readNode(start + offset + 2);
+        var ret = readNode(start+ offset + 2);
+        offset += ret.Item1;
+        childValues[i] = ret.Item2;
       }
+
+      int value = 0;
       for (int i=0; i<array[start + 1]; i++)
       {
-        int meta = array[start + offset + 2];
-        Console.Write(meta + " ");
-        metadata.Add(meta);
+        int v = array[start + offset + 2];
+        if (array[start] > 0)
+        {
+          value += (childValues.Length < v) ? 0 : childValues[v - 1];
+        }
+        else
+        {
+          value += v;
+        }
         offset++;
       }
 
-      Console.WriteLine($"Read {offset} values");
-      return offset + 2;
+      return new Tuple<int,int>(offset + 2, value);
     }
   }
 }
