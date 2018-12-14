867980767c4779f817c15fc5df9d18c01bbd7997
Matt Cosand <matt@cosand.net>
2018-12-13 21:22:36 -0800

    day 14 part 2

--- day-14/Program.cs
+++ day-14/Program.cs
@@ -8,18 +8,29 @@ namespace day_14
   {
     static void Main(string[] args)
     {
-      int input = 157901;
+      string input = "51589";
+      input = "157901";
 
       List<int> scores = new List<int>(new[] { 3, 7 } );
       int elf1 = 0;
       int elf2 = 1;
 
-      while (scores.Count < input + 10)
+      bool found = false;
+      while (!found)
       {
+        int start = Math.Max(0, scores.Count - input.Length);
         int sum = scores[elf1] + scores[elf2];
         scores.AddRange(sum.ToString().Select(c => (int)(c - '0')));
         elf1 = (elf1 + 1 + scores[elf1]) % scores.Count;
         elf2 = (elf2 + 1 + scores[elf2]) % scores.Count;
+
+        string search = string.Join("", scores.Skip(start));
+        int offset = search.IndexOf(input);
+        if (offset >= 0) {
+          int answer = start + offset;
+          Console.WriteLine(answer);
+          return;
+        }
 
         //var color = Console.ForegroundColor;
         //for (int i=0;i<scores.Count; i++)
@@ -34,8 +45,6 @@ namespace day_14
 
       }
 
-      string answer = string.Join("", scores.Skip(input).Take(10));
-      Console.WriteLine(answer);
     }
 
   }
