f18d56048de37a9a5213daae3e4768fb3a3015eb
Matt Cosand <matt@cosand.net>
2018-12-20 21:35:06 -0800

    catching up. day 19 part 1

---
+++ day-19/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day_19
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      new Program().Run();
+    }
+
+    readonly int registerCount = 6;
+    int[] registers;
+    Dictionary<string, Action<int, int, int>> instructions;
+    int instructionPointer = 0;
+    int ipBinding = 0;
+
+    private void Run()
+    {
+      registers = new int[registerCount];
+      instructions = new Dictionary<string, Action<int, int, int>>
+      {
+        { "addr", (a, b, c) => registers[c] = registers[a] + registers[b] },
+        { "addi", (a, b, c) => registers[c] = registers[a] + b },
+        { "mulr", (a, b, c) => registers[c] = registers[a] * registers[b] },
+        { "muli", (a, b, c) => registers[c] = registers[a] * b },
+        { "banr", (a, b, c) => registers[c] = registers[a] & registers[b] },
+        { "bani", (a, b, c) => registers[c] = registers[a] & b },
+        { "borr", (a, b, c) => registers[c] = registers[a] | registers[b] },
+        { "bori", (a, b, c) => registers[c] = registers[a] | b },
+        { "setr", (a, b, c) => registers[c] = registers[a] },
+        { "seti", (a, b, c) => registers[c] = a },
+        { "gtir", (a, b, c) => registers[c] = a > registers[b] ? 1 : 0 },
+        { "gtri", (a, b, c) => registers[c] = registers[a] > b ? 1 : 0 },
+        { "gtrr", (a, b, c) => registers[c] = registers[a] > registers[b] ? 1 : 0 },
+        { "eqir", (a, b, c) => registers[c] = a == registers[b] ? 1 : 0 },
+        { "eqri", (a, b, c) => registers[c] = registers[a] == b ? 1 : 0 },
+        { "eqrr", (a, b, c) => registers[c] = registers[a] == registers[b] ? 1 : 0 },
+      };
+
+      string[] input = "#ip 0\nseti 5 0 1\nseti 6 0 2\naddi 0 1 0\naddr 1 2 3\nsetr 1 0 0\nseti 8 0 4\nseti 9 0 5".Split('\n');
+      input = File.ReadAllLines("input.txt");
+      ipBinding = int.Parse(input[0].Substring(4));
+      List<Action> program = input.Skip(1).Select(f => f.Split(' ')).Select(args => (Action)(
+        () => instructions[args[0]](int.Parse(args[1]), int.Parse(args[2]), int.Parse(args[3]))
+      )).ToList();
+
+      for (int instructionPointer = 0; instructionPointer < program.Count; instructionPointer++)
+      {
+        registers[ipBinding] = instructionPointer;
+        program[instructionPointer]();
+       // Console.WriteLine($"ip={instructionPointer} [{string.Join(' ', registers)}]");
+        instructionPointer = registers[ipBinding];
+      }
+
+      int answer = registers[0];
+      Console.WriteLine(answer);
+    }
+  }
+}
